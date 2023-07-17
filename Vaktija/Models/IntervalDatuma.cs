b5ace3b93d4b5d4e83fabf46619e0486addc1117
Merima <Merima@DESKTOP-AGUG333>
2023-07-17 20:16:52 +0200

    Add project files.

---
+++ Vaktija/Models/IntervalDatuma.cs
@@ -0,0 +1,14 @@
+﻿namespace Vaktija.Models
+{
+    public struct IntervalDatuma
+    {
+        public DateTime pocetakVaktije { get; }
+        public DateTime krajVaktije { get; }
+
+        public IntervalDatuma(DateTime pocetakVaktije, DateTime krajVaktije)
+        {
+            this.pocetakVaktije = pocetakVaktije;
+            this.krajVaktije = krajVaktije;
+        }
+    }
+}
